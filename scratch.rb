90fa77f4d28ba2c6311151c50041957a5642525e
Edward Rodriguez <rodriguez.avery@gmail.com>
2024-02-15 11:44:58 -0600

    recursion practice

--- scratch.rb
+++ scratch.rb
@@ -1,1 +1,20 @@
- 
+def countdown_interative(number)
+  until number < 0
+    puts number
+    number -= 1
+  end
+end
+
+# countdown_interative(12)
+
+def countdown(n)
+  if n == 0
+    puts n
+    return
+  end
+
+  puts n
+  countdown(n - 1)
+end
+
+countdown(7)
